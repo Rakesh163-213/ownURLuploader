fbe93c495676ed76c93db5f018742a024b5e5530
Rakesh163-213 <rakeshsheokand97280@gmail.com>
2026-04-09 00:12:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
+import os
 from flask import Flask
-from threading import Thread
 
-# == Flask App ==
-flask_app = Flask(__name__)
+app = Flask(__name__)
 
-@flask_app.route('/')
+@app.route('/')
 def home():
     return '✅ Flask is running! Bot should be running too.'
 
-def run_flask():
-    flask_app.run(host='0.0.0.0', port=8000)
-
-# Start Flask in a separate thread
-flask_thread = Thread(target=run_flask)
-flask_thread.daemon = True
-flask_thread.start()
+if __name__ == "__main__":
+    # Koyeb passes a PORT variable; we use 8000 as a backup
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
+    
