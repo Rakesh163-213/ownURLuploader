a2db3746d69c640bc86e2a508cc1763ae8709362
Rakesh163-213 <rakeshsheokand97280@gmail.com>
2026-04-08 23:55:04 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+api_id = int(os.environ.get("API_ID", 0))
+api_hash = os.environ.get("API_HASH", "")
+bot_token = os.environ.get("BOT_TOKEN", "")
+
+    # Add any other settings here
+    # SESSION_NAME = os.environ.get("SESSION_NAME", "my_bot")
+  
